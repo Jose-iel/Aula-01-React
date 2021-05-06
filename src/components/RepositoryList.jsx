91eb37f2f7c197f11697bf3e9ff518534efe83f8
José <josehenriqueiel@gmail.com>
2021-05-06 13:54:34 -0300

    Incluindo API GitHub para puxar repositórios

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,28 +1,34 @@
+import { RepositoryItem } from "./RepositoryItem";
+import { useState, useEffect } from "react";
+
+import '../styles/repositories.scss';
+
+const repository ={
+    name: 'Unform',
+    description: 'Forms in React',
+    link: 'https://github.com/unform/unform'
+}
+
 export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }, []);
+
     return(
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
 
-            <ul>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">Acessar repositório</a>
-                </li>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">Acessar repositório</a>
-                </li>
-                <li>
-                    <strong>Unform</strong>
-                    <p>Forms in React</p>
-
-                    <a href="">Acessar repositório</a>
-                </li>
-            </ul>
+        <ul>
+            <RepositoryItem repository={repository}/>
+            <RepositoryItem repository={repository}/>
+            <RepositoryItem repository={repository}/>
+            <RepositoryItem repository={repository}/>
+        </ul>
         </section>
     )
 }
